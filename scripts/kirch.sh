179bf59d8735886be89ec8debb903c1498d65831
Andinus <andinus@nand.sh>
2020-08-28 01:18:31 +0530

    Fix logical error

--- scripts/kirch.sh
+++ scripts/kirch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir /tmp \
+mkdir -p /tmp \
     && git -C /tmp clone https://github.com/JKirchartz/fortunes.git kirch \
     && cd /tmp/kirch && rm -r README.md LICENSE Makefile bin \
     && cp -r /tmp/kirch/* $HOME/fortunes/
